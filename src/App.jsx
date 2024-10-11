1c29c5d9471ee3a649cc96eca089ec4e9dba4278
Ivan Palamarchuk <ipz222_pis@student.ztu.edu.ua>
2024-10-12 01:19:50 +0300

    feat: update AutoComplete demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Autocomplete from "./components/Autocomplete/Autocomplete";
 import FruitService from "./services/FruitService";
 function App() {
   const a = new FruitService().getFruits();
+  const [value, setValue] = useState("");
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Autocomplete options={a}></Autocomplete>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <span>{value}</span>
+        <Autocomplete
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          options={a}
+        ></Autocomplete>
       </div>
     </>
   );
